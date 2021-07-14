178cbf8ccbfd489a622499913ecd03407450cfb2
Joao Oliveira <joao.d.oliveira@gmail.com>
2021-07-14 11:54:37 -0300

    added Dueling option to run with a Dueling DQN

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
+    def __init__(self, state_size, action_size, seed, dueling_dqn=False, fc1_units=64, fc2_units=64):
         """Initialize parameters and build model.
         Params
         ======
@@ -19,12 +19,26 @@ class QNetwork(nn.Module):
         """
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
+        self.DUELING = dueling_dqn
+
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
+
+        if not self.DUELING:
+            self.fc3 = nn.Linear(fc2_units, action_size)
+        else:
+            self.fc3_output_v = nn.Linear(fc2_units, action_size)
+            self.fc3_output_a = nn.Linear(fc2_units, action_size)
 
     def forward(self, state):
-        """Build a network that maps state -> action values."""
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        return self.fc3(x)
+
+        if not self.DUELING:
+            """Build a network that maps state -> action values."""
+            x = self.fc3(x)
+        else:
+            x_a = self.fc3_output_a(x)
+            x = self.fc3_output_v(x) + x_a - x_a.mean(dim=1).unsqueeze(1)
+
+        return x
